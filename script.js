4ff6052f072937769490166c7c5dc1fcdfe25a08
Yamilah Atallah <yami@yamilah.com>
2015-11-22 18:46:40 -0500

    Fixed indentation inconsistency.

--- script.js
+++ script.js
@@ -24,61 +24,61 @@
     drawCircle(this.position.x, this.position.y, this.displayRadius, "black");
   };
 
-var Explosion = function(x, y) {
-  this.position = {
-    x: x,
-    y: y
+  var Explosion = function(x, y) {
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.radius = 0;
   };
-  this.radius = 0;
-};
 
-Explosion.prototype.MAX_RADIUS = 200;
+  Explosion.prototype.MAX_RADIUS = 200;
 
-Explosion.prototype.update = function(dt) {
-  this.radius.x += (this.MAX_RADIUS - this.radius)*8*dt;
+  Explosion.prototype.update = function(dt) {
+    this.radius.x += (this.MAX_RADIUS - this.radius)*8*dt;
 
-  drawCircle(this.position.x, this.position.y, this.radius, "red");
-};
+    drawCircle(this.position.x, this.position.y, this.radius, "red");
+  };
 
-var drawCircle = function(x, y, radius, fillStyle) {
-  context.beginPath();
-  context.fillStyle = fillStyle;
-  context.arc(x, y, radius, 0, Math.PI*2);
-  context.closePath();
-  context.fill();
-};
+  var drawCircle = function(x, y, radius, fillStyle) {
+    context.beginPath();
+    context.fillStyle = fillStyle;
+    context.arc(x, y, radius, 0, Math.PI*2);
+    context.closePath();
+    context.fill();
+  };
 
-// Global state.
-var init = function() {
-  canvas = document.getElementById("canvas");
-  context = canvas.getContext("2d");
+  // Global state.
+  var init = function() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
 
-  currentTime = Date.now();
+    currentTime = Date.now();
 
-  mouse = {
-   x: canvas.width/2,
-   y: canvas.height/2,
-   pressed: false
- };
+    mouse = {
+      x: canvas.width/2,
+      y: canvas.height/2,
+      pressed: false
+    };
 
-  canvas.addEventListener("mousemove", function(event) {
-    var canvasRect = canvas.getBoundingClientRect();
-    mouse.x = event.clientX - canvasRect.x;
-    mouse.y = event.clientY - canvasRect.y;
-  });
+    canvas.addEventListener("mousemove", function(event) {
+      var canvasRect = canvas.getBoundingClientRect();
+      mouse.x = event.clientX - canvasRect.x;
+      mouse.y = event.clientY - canvasRect.y;
+    });
 
-  canvas.addEventListener("mousedown", function(event) {
-    mouse.pressed = true;
-  });
+    canvas.addEventListener("mousedown", function(event) {
+      mouse.pressed = true;
+    });
 
-  canvas.addEventListener("mouseup", function(event) {
-    mouse.pressed = false;
-  });
+    canvas.addEventListener("mouseup", function(event) {
+      mouse.pressed = false;
+    });
 
-  player = new Player(canvas.width/2, canvas.height/2);
+    player = new Player(canvas.width/2, canvas.height/2);
 
-  requestAnimationFrame(update);
-};
+    requestAnimationFrame(update);
+  };
 
   var update = function() {
     previousTime = currentTime;
@@ -95,6 +95,6 @@ var init = function() {
     requestAnimationFrame(update);
   };
 
-init();
+  init();
 
 })();
